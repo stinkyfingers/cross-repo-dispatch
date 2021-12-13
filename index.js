61b29b76d8791e3f413547af303039c6f11dd4b2
stinkyfingers <john_shenk@hotmail.com>
2021-12-13 09:40:53 -0600

    http

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fetch = require('node-fetch');
+const http = require('http');
 
 try {
   const owner = core.getInput('owner');
