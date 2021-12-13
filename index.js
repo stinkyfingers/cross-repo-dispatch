3dfcbecc86f4edf2ffbd79f2c04e1ceedb1f74c8
stinkyfingers <john_shenk@hotmail.com>
2021-12-13 09:49:30 -0600

    http

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const http = require('http');
+const https = require('http');
 
 try {
   const owner = core.getInput('owner');
@@ -8,24 +8,30 @@ try {
   const pat = core.getInput('pat');
   const sha = core.getInput('sha');
 
-  const req = http.request({
+  const req = https.request({
     hostname: 'api.github.com',
     path: `/repos/${owner}/${repo}/actions/runs`,
     method: 'GET',
-
+    headers: {
+      'Content-Type': 'application/json',
+      'accept': 'application/vnd.github.everest-preview+json',
+      'Authorization': 'Basic ' + new Buffer(owner + ':' + pat).toString('base64')
+    }
   }, res => {
     res.on('data', d => {
       core.setOutput('response', d);
     })
-  }).end();
+  });
 
   req.on('error', error => {
     throw new Error(error);
   });
 
+  req.end();
+
   core.setOutput('status', 'SUCCESSING');
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
