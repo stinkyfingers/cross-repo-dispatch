b4457023a18d9bf15765ce0d62b0591bb6987190
stinkyfingers <john_shenk@hotmail.com>
2021-12-13 09:36:04 -0600

    http req

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fetch = require('node-fetch');
 
-try {
-  const owner = core.getInput('owner');
-  const repo = core.getInput('repo');
-  const pat = core.getInput('pat');
-  const sha = core.getInput('sha');
 
-  core.setOutput('status', 'SUCCESSING');
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const owner = core.getInput('owner');
+    const repo = core.getInput('repo');
+    const pat = core.getInput('pat');
+    const sha = core.getInput('sha');
+
+    const resp = await fetch(`https://api.github.com/repos/${owner}/${repo}/actions/runs`);
+    const results = await resp.json();
+    core.setOutput('results', results)
+
+    core.setOutput('status', 'SUCCESSING');
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
